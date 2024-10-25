e20c973b455d94beaea1f7a09fd72d97370fea5c
mihaiconstantin <mihai@mihaiconstantin.com>
2024-10-25 19:23:27 +0200

    Build: remove redundant `rlang` imports

--- R/parSim-package.R
+++ R/parSim-package.R
@@ -2,7 +2,6 @@
 #' @importFrom dplyr %>% filter bind_rows left_join
 #' @importFrom parabar get_option set_option start_backend stop_backend
 #' @importFrom parabar export evaluate par_lapply BarFactory
-#' @importFrom rlang !!! as_quosures
 
 
 # Re-exported functions for convenience.
