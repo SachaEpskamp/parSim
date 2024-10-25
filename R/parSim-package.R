c4e57b042df41c59b17bddd6d4985f8f36bef9ae
mihaiconstantin <mihai@mihaiconstantin.com>
2024-10-25 17:48:19 +0200

    Build: add package file with `roxygen2` imports

---
+++ R/parSim-package.R
@@ -0,0 +1,5 @@
+# Imports.
+#' @importFrom dplyr %>% filter bind_rows left_join
+#' @importFrom parabar get_option set_option start_backend stop_backend
+#' @importFrom parabar export evaluate par_lapply BarFactory
+#' @importFrom rlang !!! as_quosures
